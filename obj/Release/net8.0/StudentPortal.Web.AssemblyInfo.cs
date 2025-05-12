8f2585c6c338c9a57eea1f871ea7f95c3c921c30
parasdevop <paras.verma@arisein.com>
2025-05-12 15:47:00 +0530

    button added

--- obj/Release/net8.0/StudentPortal.Web.AssemblyInfo.cs
+++ obj/Release/net8.0/StudentPortal.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e057dca5af96b28b3ff3ff60ee8a20e0b3ff336")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
